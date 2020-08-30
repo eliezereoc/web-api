bb7df518c9412e3afe1f413858cbe66e8cb860e3
Elezer de Oliveira <eliezeroc@gmail.com>
2020-08-30 09:44:55 -0300

    Inicio aplicação Rest-api

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const express = require("express");
+const { response } = require("express");
+
+
+const app = express();
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+app.set("port", port);
+
+app.use((request,response,next) => {
+    response.status(404).send();
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor em execução em http://${hostname}:${port}`)
+}); 
